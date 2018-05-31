7c21f19a9c1100fd5e65884c32c6569d0a761b8c
hasans3 <saqib.hasan@vanderbilt.edu>
2018-05-31 14:11:12 -0500

    Update Tree_Functions.java

--- Trees_and_Graphs/Tree_Functions.java
+++ Trees_and_Graphs/Tree_Functions.java
@@ -3,6 +3,7 @@ public class Tree_Functions {
 	Node root;
 	public int count = 0;
 	
+	// Method to add a node to the tree
 	public Node AddElement(Node current, int value){
 		if (current == null){
 			Increment_count();
@@ -22,18 +23,22 @@ public class Tree_Functions {
 		return current;
 	}
 	
+	// Method to start with the root element
 	public void add(int value){
 		root = AddElement(root, value);
 	}
 	
+	// Method to increment the node count
 	public void Increment_count(){
 		count ++;
 	}
 	
+	// Method to get the node count
 	public int Get_count(){
 		return count;
 	}
 	
+	// Method to find if a node is in a tree
 	public boolean FindingElement(Node current, int value){
 		if (current == null){
 			return false;
@@ -44,6 +49,7 @@ public class Tree_Functions {
 		return value < current.data ? FindingElement(current.left, value) : FindingElement(current.right, value);
 	}
 
+	// Method to return the status of the element 
 	public boolean FindElement(int value){
 		boolean status = FindingElement(root, value);
 		System.out.println("Element Status: " + status);
